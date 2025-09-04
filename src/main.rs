486e7ce598aaa7df96416b136575c45531641d9d
Vishnu Vardhan Vankayalapati <vankayalapativv.dev@gmail.com>
2025-09-05 05:08:48 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Vishnu Vardhan!");
 }
+
+// This lines define a function in Rust. The main function is special, It is always the first code that runs in every executable Rust Program. 
+// The first line declares a function named 'main' that has no parameters and returns nothing.
+// If there have the parameters, they would be defined within the parantheses.
+// Second, println! calls a Rust macro. 
